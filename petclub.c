edad51ff182491fbd6e032df29b2ae4ddb9a8c35
linzhenghao233 <1392872316@qq.com>
2023-10-30 22:26:58 +0800

    1030

--- petclub.c
+++ petclub.c
@@ -99,10 +99,53 @@ void findpet(const Tree* pt) {
 	s_gets(temp.petkind, SLEN);
 	uppercase(temp.petname);
 	uppercase(temp.petkind);
-	printf("%s the %s", tmep.petname, temp.petkind);
+	printf("%s the %s", temp.petname, temp.petkind);
 	if (InTree(&temp, pt))
 		printf("is a member.\n");
 	else
 		printf("is not a member.\n");
 }
 
+void droppet(Tree* pt) {
+	Item temp;
+
+	if (TreeIsEmpty(pt)) {
+		puts("No entries!");
+		return;
+	}
+
+	puts("Please enter name of pet you wish to delete:");
+	s_gets(temp.petname, SLEN);
+	puts("Please enter pet kind:");
+	s_gets(temp.petkind, SLEN);
+	uppercase(temp.petname);
+	uppercase(temp.petkind);
+	printf("%s the %s ", temp.petname, temp.petkind);
+	if (DeleteItem(&temp, pt))
+		printf("is dropped from the club.\n");
+	else
+		printf("is not a member.\n");
+}
+
+void uppercase(char* str) {
+	while (*str) {
+		*str = toupper(*str);
+		str++;
+	}
+}
+
+char* s_gets(char* st, int n) {
+	char* ret_val;
+	char* find;
+
+	ret_val = fgets(st, n, stdin);
+	if (ret_val) {
+		find = strchr(st, '\n');
+		if (find)
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;
+	}
+	return ret_val;
+}
